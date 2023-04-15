a2059a9fe601b2bd46d3db26eed7a3473c00b4bd
Joao-Branco <jpbranco@academiafa.edu.pt>
2023-04-15 14:32:04 +0100

    problems solved; need correct problems with rosbag of target and playing it back

--- scripts/simulation/sim.py
+++ scripts/simulation/sim.py
@@ -5,37 +5,19 @@ import os
 import signal
 from pathlib import Path
 
-from plot_single import run_single_plots
-from plot_multi import run_multi_plots
+from plot_auto import run_auto_plots
 
-SIMTIME = 10
+uav_total = 5
+
+bag_fn = r'/home/branco/catkin_ws/src/sensor_fusion/sims/sim-multi1681381667/multi1681381667.bag'
 SIM_ID = int(time.time())
+
 sim_dir = Path(f"/home/branco/catkin_ws/src/sensor_fusion/sims/sim-single{SIM_ID}")
 sim_dir.mkdir()
 
-bag_fn_single = sim_dir.joinpath(f"single{SIM_ID}.bag")
 
-cmd_launch_single = "roslaunch sensor_fusion single_filters.launch"
-cmd_bag_single = f"rosbag record -a -O {str(bag_fn_single)}"
-
-p_launch_single = sp.Popen(cmd_launch_single.split())
-
-p_bag_single = sp.Popen(cmd_bag_single.split())
-
-t_0 = time.time()
-
-while time.time() - t_0 < SIMTIME :
-    continue
-
-
-os.kill(p_launch_single.pid, signal.SIGINT)
-
-os.kill(p_bag_single.pid, signal.SIGINT)
-
-time.sleep(2)
-
-
-run_single_plots(str(bag_fn_single), folder=str(sim_dir))
+single = True
+run_single_plots(str(bag_fn), uav_total, single, folder=str(sim_dir))
 
 
 print("Now MULTI")
@@ -44,26 +26,5 @@ SIM_ID = int(time.time())
 sim_dir = Path(f"/home/branco/catkin_ws/src/sensor_fusion/sims/sim-multi{SIM_ID}")
 sim_dir.mkdir()
 
-bag_fn_multi = sim_dir.joinpath(f"multi{SIM_ID}.bag")
-
-cmd_launch_multi = "roslaunch sensor_fusion multi_filters.launch"
-cmd_bag_multi = f"rosbag record -a -O {str(bag_fn_multi)}"
-
-p_launch_multi = sp.Popen(cmd_launch_multi.split())
-
-p_bag_multi = sp.Popen(cmd_bag_multi.split())
-
-t_0 = time.time()
-
-while time.time() - t_0 < SIMTIME :
-    continue
-
-
-os.kill(p_launch_multi.pid, signal.SIGINT)
-
-os.kill(p_bag_multi.pid, signal.SIGINT)
-
-time.sleep(2)
-
-
-run_multi_plots(str(bag_fn_multi), folder=str(sim_dir))
+single = False
+run_multi_plots(str(bag_fn), folder=str(sim_dir), uav_total, single)
