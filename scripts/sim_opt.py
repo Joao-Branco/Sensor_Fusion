844da98f046f99826670aefb2fcc53cf8baf3d02
Joao-Branco <jpbranco@academiafa.edu.pt>
2023-06-16 12:56:52 +0100

    working in numerical simulation

--- scripts/sim_opt.py
+++ scripts/sim_opt.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math
-from kalman_filter import KalmanFilter, f_nonlinear, Jacobian
+from kalman_filter import KalmanFilter
 import time as pytime
 import matplotlib.pyplot as plt
 
@@ -16,6 +16,7 @@ mean = 0
 n_uavs = 3
 
 SHARE_ON = True
+EKF = True
 
 
 time = np.arange(0, sim_time, 1/f)
@@ -23,23 +24,29 @@ time = np.arange(0, sim_time, 1/f)
 print(time, len(time))
 
 def target_dynamics(t):
-    x = 5 * np.cos( 0.09 * t) 
-    v_x =  -0.45 * np.sin(0.09 * t)
-
-    y = 5 * np.sin( 0.09 * t)
-    v_y = 0.45 * np.cos(0.09 * t)
-    return x,y,v_x,v_y
+    v = 5
+    w = 0.09
+    theta = w * t
+    v_x = v * np.cos(theta)
+    v_y = v * np.sin(theta)
+    x = v_x
+    y = v_y
+    
+    return x,y,v_x,v_y, v, theta, w
 
 def target_dynamics_sin(t):
-    x = 5 * np.cos( 0.2 * t) 
-    v_x =  -0.1 * np.sin(0.2 * t)
-
-    y = 1 * t
+    v = 5
+    w = 0.09
+    theta = w * t
+    v_x = v * np.cos(theta)
     v_y = 1
-    return x,y,v_x,v_y
+    x = v_x
+    y = v_y
+    
+    return x,y,v_x,v_y, v, theta, w
         
 
-x,y,vx,vy = target_dynamics_sin(time)
+x,y,vx,vy, vv, tt, ww = target_dynamics(time)
 print(x, len(vx))
 
 def gen_sensor_data(*arrays):
@@ -68,8 +75,13 @@ sensors = [gen_sensor_data(x,y) for i in range(n_uavs)]
 dt = 1 / f_kf
 
 
-x0 = np.array([    [5],
+x0 = np.array([     [5],
                     [0],
+                    [0],
+                    [0],
+                    [0]])
+
+x0_KF = np.array([  [5],
                     [0],
                     [0],
                     [0]])
@@ -78,57 +90,77 @@ x0 = np.array([    [5],
 
 # x ----Initial value for the state
 
-F = np.array([     [1, 0, dt, 0],
-                            [0, 1, 0, dt],
-                            [0, 0, 1, 0],
-                            [0, 0, 0, 1]])
+F = np.array([      [1, 0, dt, 0],
+                    [0, 1, 0, dt],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]])
 
 
 # F ----State trasition(A) (Dynamic Model)
 
-H = np.array([     [1, 0, 0, 0, 0],
-                        [0, 1, 0, 0, 0]])
+H = np.array([      [1, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0]])
+
+H_KF = np.array([   [1, 0, 0, 0],
+                    [0, 1, 0, 0]])
 
 
-H_fuse = np.array([    [1, 0, 0, 0, 0],
-                            [0, 1, 0, 0, 0],
-                            [0, 0, 1, 0, 0],
-                            [0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 1]])
+H_fuse = np.array([     [1, 0, 0, 0, 0],
+                        [0, 1, 0, 0, 0],
+                        [0, 0, 1, 0, 0],
+                        [0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 1]])
+
+H_fuse_KF = np.array([  [1, 0, 0, 0],
+                        [0, 1, 0, 0],
+                        [0, 0, 1, 0],
+                        [0, 0, 0, 1]])
 
 
-Q = np.array([     [0.5, 0, 0, 0, 0],
-                        [0, 0.5, 0, 0, 0],
-                        [0, 0, 0.1, 0, 0],
-                        [0, 0, 0, 0.5, 0],
-                        [0, 0, 0, 0, 0.1]])
-Q *=0.1
+Q = np.array([      [0.5, 0, 0, 0, 0],
+                    [0, 0.5, 0, 0, 0],
+                    [0, 0, 0.1, 0, 0],
+                    [0, 0, 0, 0.5, 0],
+                    [0, 0, 0, 0, 0.1]])
+
+Q_KF = np.array([   [0.5, 0, 0, 0],
+                    [0, 0.5, 0, 0],
+                    [0, 0, 0.1, 0],
+                    [0, 0, 0, 0.1]])
+
 
 # Q ----Process Noise
 
 R = np.array([ [2, 0],
                [0, 2]])
 
-R_fuse = np.array([    [0.5, 0, 0, 0, 0],
-                            [0, 0.5, 0, 0, 0],
-                            [0, 0, 0.1, 0, 0],
-                            [0, 0, 0, 1, 0],
-                            [0, 0, 0, 0, 0.1]])
-R_fuse *=0.1
+R_fuse = np.array([     [0.5, 0, 0, 0, 0],
+                        [0, 0.5, 0, 0, 0],
+                        [0, 0, 0.1, 0, 0],
+                        [0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 0.1]])
 
-# R_fuse = np.array([[ 0.6226917 ,  0.09437438 , 0.592893  ,  0.2505855 ,  0.14878439],
-#  [ 0.09437438 , 0.5871143 ,  0.41238568,  0.29699649 , 0.10378804],
-#  [ 0.592893  ,  0.41238568  ,3.73216838, -0.14775829 , 1.07061657],
-#  [ 0.2505855,   0.29699649 ,-0.14775829 , 3.03817689 ,-0.03289477],
-#  [ 0.14878439 , 0.10378804 , 1.07061657 ,-0.03289477 , 0.67679524]])
+
+R_fuse_KF = np.array([  [0.5, 0, 0, 0],
+                        [0, 0.5, 0, 0],
+                        [0, 0, 0.1, 0],
+                        [0, 0, 0, 1]])
+
+
 
 # R ----Measurement Noise
 
-
-kfs = [KalmanFilter(F = F.copy(), H = H.copy(),
+if (EKF == True):
+    kfs = [KalmanFilter(F = F.copy(), H = H.copy(),
                     H_fuse = H_fuse.copy() , Q = Q.copy() ,
                     R = R.copy(), R_fuse = R_fuse.copy(),
                     x0= x0.copy(), dt = dt) for i in range(n_uavs)]
+else:
+    kfs = [KalmanFilter(F = F.copy(), H_KF = H_KF.copy(),
+                    H_fuse_KF = H_fuse_KF.copy() , Q_KF = Q_KF.copy() ,
+                    R = R.copy(), R_fuse_KF = R_fuse_KF.copy(),
+                    x0_KF = x0_KF.copy(), dt = dt) for i in range(n_uavs)]
+    
 
 
 # matrix for results
@@ -173,7 +205,11 @@ for i, t in enumerate(time):
     # predict
     if t-t_predict >= p_predict:
         for uav_i, kf in enumerate(kfs):
-            x_i = kf.predict_nonlinear()
+            if (EKF == True):
+                x_i = kf.predict_nonlinear()
+            else:
+                x_i = kf.predict()
+                
             predicts[uav_i][0,col_write] = t
             predicts[uav_i][1:,col_write] = x_i[:,0]
         col_write += 1
@@ -199,15 +235,20 @@ print(kfs[0].P)
 plt.plot(x,y, 'k')
 for i in range(n_uavs):
     x_, y_ = predicts[i][1,:], predicts[i][2,:]
-    plt.plot(x_[:col_write], y_[:col_write])
+    #plt.plot(x_[:col_write], y_[:col_write])
+plt.grid()
 plt.show()
 
 print(predicts[0][1,-1])
 
-#for t in enumerate(time):
-    #for uav_i in enumerate(kfs):
-        #if t == predicts[uav_i][0,t]:
-        #error_x[uav_i] = abs(x - predicts[uav_i][1,t])
-        #error_y[uav_i] = abs(x - predicts[uav_i][1,t])
+for i in enumerate(time):
+    for uav_i in enumerate(kfs):
+        if t == predicts[uav_i][0,t]:
+            error = abs(x - predicts[uav_i][1,t])
+            
+            
+print(error[0][0,:])
+
+
 
 
