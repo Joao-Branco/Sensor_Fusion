020ba13f4eec5bb95235a3fc3766a88162e4fb38
Joao-Branco <jpbranco@academiafa.edu.pt>
2023-08-10 16:16:02 +0100

    need to get the dynamics right

--- scripts/target_dynamics.py
+++ scripts/target_dynamics.py
@@ -6,19 +6,19 @@ def circular_path(t):
     v = 6 * np.ones_like(t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
 def sin_path(t):
-    w = 2 * np.sin(0.3 * t)
+    w = 1 * np.ones_like(t)
     h = w * t
-    v = 6 * np.ones_like(t)
+    v = 6 * np.cos(0.8 * t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
@@ -26,12 +26,12 @@ def sin_path(t):
 
 def linear_path(t):
     w = 0 * np.ones_like(t)
-    h = w * t - 1
-    v = -6
+    h = w * t + 0.3
+    v = 6 * np.ones_like(t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
@@ -42,19 +42,19 @@ def circular_xy_path(t):
     v = 6
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
 def sin_xy_path(t):
-    w = 5 * np.cos(0.1 * t)
+    w = 0.5 * np.ones_like(t)
     h = w * t
-    v = 6 * np.ones_like(t)
+    v = 6 * np.sin(0.8 * t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
@@ -62,34 +62,34 @@ def sin_xy_path(t):
 
 def linear_xy_path(t):
     w = 0 * np.ones_like(t)
-    h = w * t
+    h = w * t - 0.8
     v = 6
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
 def stoped_xy_path(t):
-    w = 0.5 * np.ones_like(t)
+    w = 0 * np.ones_like(t)
     h = w * t
     v = 0 * np.ones_like(t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t
-    y = v_y * t
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
 
 
 def stoped_path(t):
-    w = 0.5 * np.ones_like(t)
+    w = 0 * np.ones_like(t)
     h = w * t
     v = 0 * np.ones_like(t)
     v_x = v * np.cos(h)
     v_y = v * np.sin(h) 
-    x = v_x * t + 200
-    y = v_y * t - 200
+    x = v * np.sin(h) 
+    y = - v * np.cos(h)
     
     return x, y, v_x, v_y, w
